6fa8deaadff28e9fb286eafdd87a44504096432b
Robson Kades <robsonkades@outlook.com>
2023-10-19 22:36:50 -0300

    initial commit

---
+++ spring-cloud-robsonkades-nosqlpersister/src/main/java/com/robsonkades/spring/nosqlpersister/NoSQLPersister.java
@@ -0,0 +1,8 @@
+package com.robsonkades.spring.nosqlpersister;
+
+public class NoSQLPersister {
+
+    public void execute(String message) {
+        System.out.println(message);
+    }
+}
---
+++ spring-cloud-robsonkades-sqlpersister/src/main/java/com/robsonkades/spring/sqlpersister/SQLPersister.java
@@ -0,0 +1,8 @@
+package com.robsonkades.spring.sqlpersister;
+
+public class SQLPersister {
+
+    public void execute(String message) {
+        System.out.println(message);
+    }
+}
